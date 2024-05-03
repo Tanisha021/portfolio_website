eeeff7b2a3aa043d410c133157d0b269c7ca2b10
Tanisha021 <tanishaagrawal021@gmail.com>
2024-05-04 00:31:15 +0530

    profile photo

--- src/assets/index.js
+++ src/assets/index.js
@@ -8,17 +8,23 @@ import contactImg from "./images/contact/contactImg.png";
 import me3 from "./images/me3.png";
 import me1 from "./images/me1.png";
 import me2 from "./images/me2.png";
+import pic from "./images/pic.jpeg";
+import pic2 from "./images/pic2.jpeg";
+import me from "./images/me.png";
 import face_logo_1 from "./images/face_logo_1.png"
 import github from "./images/projects/github.png"
 
 // import screen from "./images/screen.svg"
 
 export {
+  pic2,
+  pic,
   github,
   face_logo_1,
+  me,
   me1,
-  me2,
   me3,
+  me2,
   bannerImg,
   projectOne,
   projectTwo,
